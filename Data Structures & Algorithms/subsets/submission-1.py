70afd97801cbe13626cfa116fe6b8d3258c467c7
Andrew Velis <avelis@users.noreply.github.com>
2026-05-03 22:41:16 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,7 @@
+from itertools import combinations
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for num in nums:
+            res += [subset + [num] for subset in res]
+        return res
